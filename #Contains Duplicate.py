6ce789bc4634a7389b234dc8552cee1082df322d
Viet Thang <vietthangpq099@gmail.com>
2026-01-26 20:30:48 +0700

    init project

---
+++ #Contains Duplicate.py
@@ -0,0 +1,9 @@
+#Contains Duplicate
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
