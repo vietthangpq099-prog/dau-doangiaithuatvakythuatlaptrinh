b3f98e6c8c8a88ae8e3592529ae52a63e05fabba
Viet Thang <vietthangpq099@gmail.com>
2026-03-03 08:15:28 +0700

    bai 20

---
+++ Word Pattern.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = {}
+        word_to_char = {}
+        
+        for c, w in zip(pattern, words):
+            if c in char_to_word:
+                if char_to_word[c] != w:
+                    return False
+            else:
+                char_to_word[c] = w
+            
+            if w in word_to_char:
+                if word_to_char[w] != c:
+                    return False
+            else:
+                word_to_char[w] = c
+        
+        return True
+        
