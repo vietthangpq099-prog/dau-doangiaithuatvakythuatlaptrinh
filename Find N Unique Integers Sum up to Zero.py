718d8ad2b4ef3c1092683fbf19e4780a7a9b6b03
Viet Thang <vietthangpq099@gmail.com>
2026-03-09 21:24:30 +0700

    bai 31

---
+++ Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sumZero(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = []
+        if n % 2 != 0:
+            result.append(0)
+        for i in range(1, (n // 2) + 1):
+            result.append(i)
+            result.append(-i)
+        return result
