e69a597526f21e9d1f25553f43610ac27461937b
Viet Thang <vietthangpq099@gmail.com>
2026-03-10 08:47:37 +0700

    bai 37

---
+++ Lemonade Change.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five_count =0
+        ten_count = 0
+        for bill in bills:
+            if bill == 5:
+                five_count += 1
+            elif bill == 10:
+                if five_count > 0:
+                    five_count -=1
+                    ten_count +=1
+                else:
+                    return False
+            elif bill ==20:
+                if ten_count > 0 and five_count >0:
+                    ten_count -=1
+                    five_count -=1
+                elif five_count >=3:
+                    five_count -=3
+                else:
+                    return False
+        return True
+        
