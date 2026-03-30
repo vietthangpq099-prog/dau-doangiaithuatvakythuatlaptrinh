6c466adb8fc1fe0f873dff4cf0b623b3b08279d0
Viet Thang <vietthangpq099@gmail.com>
2026-03-30 18:14:22 +0700

    bai 69

---
+++ 2169. Count Operations to Obtain Zero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countOperations(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        dem_buoc = 0
+        # Lặp chừng nào cả hai số đều chưa về 0
+        while num1 > 0 and num2 > 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            dem_buoc += 1
+        return dem_buoc
