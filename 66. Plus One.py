9283cad0d15a0888204318c4dd30602f846d17b5
Viet Thang <vietthangpq099@gmail.com>
2026-04-20 20:44:38 +0700

    115

---
+++ 66. Plus One.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+ 
+            digits[i] = 0
+        return [1] + digits
